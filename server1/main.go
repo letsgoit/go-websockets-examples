7bf4b27ab15e2e86b68a0be056a575c59183913a
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-02-22 23:21:43 +0530

    Simple Websocket Example : server1

---
+++ server1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+var upgrader = websocket.Upgrader{}
+
+func echo(w http.ResponseWriter, r *http.Request){
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade: ", err)
+		return
+	}
+
+	defer c.Close()
+
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read: ", err)
+			return
+		}
+
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write: ", err)
+			break
+		}
+	}
+}
+
+func main(){
+	http.HandleFunc("/",echo)
+	http.ListenAndServe(*addr, nil)
+}
